46bcb9116aed6295acba333a4353f442ca9679c3
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-11-16 12:19:14 +0900

    🔧 Adapted '@ts-check' and JSDoc to build.js

--- build.js
+++ build.js
@@ -1,11 +1,16 @@
+// @ts-check
 const { build } = require('esbuild')
-const { dependencies } = require('./package.json')
+const packageJson = require('./package.json')
 
+// @ts-ignore Checked Object keys
+const dependencies = packageJson.hasOwnProperty('dependencies') ? packageJson.dependencies : {}
 const entryFile = 'src/index.ts'
+
+/** @type {import('esbuild').BuildOptions} */
 const shared = {
   bundle: true,
   entryPoints: [entryFile],
-  external: Object.keys(dependencies || {}),
+  external: Object.keys(dependencies),
   logLevel: 'info',
   minify: true,
   sourcemap: false,
@@ -15,8 +20,8 @@ build({
   ...shared,
   format: 'esm',
   outfile: './dist/index.esm.js',
-  target: ['ES6'],
-})
+  target: ['ES6']
+});
 
 build({
   ...shared,
